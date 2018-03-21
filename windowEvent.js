95bdfee64caea7bd6f1038ed1569dc8e4f1fbd32
Connor <cdy@email.sc.edu>
2018-03-21 14:03:33 -0400

    populate number of rounds options

--- windowEvent.js
+++ windowEvent.js
@@ -10,15 +10,21 @@ class WindowEvent extends React.Component
 
     render()
     {
+        var roundOptions = [];
+        for (var i = 1; i <= 30; i++)
+        {
+            roundOptions.push(<option key={i-1} value={i}>{i}</option>);
+        }
+
         return(<div>
-            Event Name: <input type="text" id="eventName" /><br class="tall" />
-            Number of Rounds: <select id="numRounds"></select><br class="tall" />
+            Event Name: <input type="text" id="eventName" /><br className="tall" />
+            Number of Rounds: <select id="numRounds">{roundOptions}</select><br className="tall" />
             Match Type:
             <select id="matchType">
                 <option value="0">1v1</option>
                 <option value="1">Free-for-All</option>
                 <option value="2">Teams</option>
-            </select><br class="tall" />
+            </select><br className="tall" />
             <div id="defaultTeamPodSize">
                 Default Team/Pod Size: <select id="podSize"></select>
             </div>
