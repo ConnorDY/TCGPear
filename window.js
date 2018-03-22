0c0f3d0082a29521b2c4c6a2341160e5000085d9
Connor <cdy@email.sc.edu>
2018-03-21 23:15:41 -0400

    first gh-pages build

--- window.js
+++
@@ -1,9 +0,0 @@
-import React from "react";
-
-const Window = ({ name, isActive, children }) => (
-<div id={name} className={"tab" + (isActive ? "" : " hidden")}>
-    {children}
-</div>
-);
-
-export default Window;
